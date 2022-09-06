9dc02ceba1cfc97a3fd5d5c7391d8f7803a2ea7a
jrobledo <robledo.josej@gmail.com>
2022-09-06 11:01:01 -0600

    wip - updating popup styling

--- src/components/MarkerDescriptions.component.js
+++ src/components/MarkerDescriptions.component.js
@@ -30,53 +30,58 @@ export default function MarkerDescriptions({ props }) {
       >
         <div className="popup">
           {editing ? (
-            <div>
-              <form onSubmit={handleUpdatePost}>
-                <label>Location</label>
-                <input
-                  placeholder="Location"
-                  defaultValue={post.title}
-                  onChange={(e) => setNewLocation(e.target.value)}
-                />
-                <label>Description</label>
-                <textarea
-                  placeholder="description"
-                  defaultValue={post.description}
-                  onChange={(e) => setNewDescription(e.target.value)}
-                />
-                <button
-                  type="submit"
-                  className="button btn-post-submit"
-                  onClick={() => {
-                    setEditId(post._id);
-                  }}
-                >
-                  Update
-                </button>
-                <button
-                  type="submit"
-                  className="button btn-post-submit"
-                  onClick={() => {
-                    setEditing(false);
-                  }}
-                >
-                  Cancel
-                </button>
-              </form>
-            </div>
-          ) : (
-            <div>
-              <label>{post.title}</label>
-
+            <form onSubmit={handleUpdatePost}>
+              <label>Location</label>
+              <input
+                placeholder="Location"
+                defaultValue={post.title}
+                onChange={(e) => setNewLocation(e.target.value)}
+              />
               <label>Description</label>
-              <p className="description">{post.description}</p>
-              <label>Created By:</label>
-              <span className="created-by">{post.username}</span>
-              <span className="date">{"3 months ago"}</span>
+              <textarea
+                placeholder="description"
+                defaultValue={post.description}
+                onChange={(e) => setNewDescription(e.target.value)}
+              />
+              <button
+                type="submit"
+                className="button btn-post-submit"
+                onClick={() => {
+                  setEditId(post._id);
+                }}
+              >
+                Update
+              </button>
+              <button
+                type="submit"
+                className="button btn-post-submit"
+                onClick={() => {
+                  setEditing(false);
+                }}
+              >
+                Cancel
+              </button>
+            </form>
+          ) : (
+            <div className="popup-content">
+              <div className="popup-div">
+                <label className="post-title">{post.title}</label>
+              </div>
+              <div className="popup-div">
+                <label>Description:</label>
+                <p className="description">{post.description}</p>
+              </div>
+              <div className="popup-div">
+                <label>Created By:</label>
+                <span className="created-by">{post.username}</span>
+                <span className="date">{post.createdAt}</span>
+              </div>
             </div>
           )}
-          <DeleteIcon onClick={() => handleDelete(post._id)} />
-          <BorderColorIcon onClick={() => setEditing(true)} />
+          <div className="post-edit-icons">
+            <DeleteIcon onClick={() => handleDelete(post._id)} />
+            <BorderColorIcon onClick={() => setEditing(true)} />
+          </div>
         </div>
       </Popup>
     );

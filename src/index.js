856bfc09ca9cf67e7173bc46088c174e83c36da2
jrobledo <robledo.josej@gmail.com>
2022-09-05 23:47:12 -0600

    added auth context and post context to app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./context/UserAuth.context";
+import { PostsContextProvider } from "./context/Posts.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <PostsContextProvider>
+        <App />
+      </PostsContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
